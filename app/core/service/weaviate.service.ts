8d13d2350c582d303079a3d07c99a823b1ae643b
Suraj Thapa <surajthapa@Surajs-MacBook-Pro.local>
2024-12-25 21:36:34 +0545

    Default connection params for local

--- app/core/service/weaviate.service.ts
+++ app/core/service/weaviate.service.ts
@@ -1,10 +1,27 @@
 import weaviate, {WeaviateClient} from 'weaviate-client';
 
 class WeaviateService {
-    weaviate: WeaviateClient|null = null
+    weaviate: WeaviateClient | null = null
 
     async getWeaviateClient() {
+
+
         this.weaviate = await weaviate.connectToLocal();
+        /**
+         * DEFAULT CONNECTION PARAMS FOR LOCAL
+         */
+        //connectionParams: {
+        //     http: {
+        //         secure: false,
+        //             host: options?.host || 'localhost',
+        //             port: options?.port || 8080,
+        //     },
+        //     grpc: {
+        //         secure: false,
+        //             host: options?.host || 'localhost',
+        //             port: options?.grpcPort || 50051,
+        //     },
+        // },
         const clientReadiness = await this.weaviate.isReady();
         if (clientReadiness) {
             return this.weaviate
